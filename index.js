15c5ea31dc3f6174a916880bfb47b422dca5c058
Paul Huszak <paulhuszak@gmail.com>
2022-10-03 13:37:38 +0200

    Changed to bcryptjs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const https = require("https");
 const fs = require("fs");
 
